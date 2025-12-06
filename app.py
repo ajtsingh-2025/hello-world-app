74a4fa79789407a87a30fc0b14aab0b7c1d4418e
ajtsingh-2025 <ajtsing@hotmail.com>
2025-12-06 08:13:29 +0000

    test

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 
 from flask import Flask
 
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+def my_function():
+    # This is a local variable
+    local_variable = "I am local to my_function"
+    print(local_variable)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
